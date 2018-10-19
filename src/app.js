ca47a04377d200d9d8f0f4feab63f193ba419b69
Akitektuo <alexcopindean@yahoo.com>
2018-10-19 10:51:27 +0300

    Add firebase test

--- src/app.js
+++ src/app.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
 import './app.css';
+import firebase from 'firebase';
 
 class App extends Component {
+
+  componentWillMount() {
+    this.setState({
+      imageURL: "",
+      name: "",
+      description: ""
+    });
+    if (!firebase.apps.length) {
+      var config = {
+        databaseURL: "https://mariana-bojan.firebaseio.com/",
+        projectId: "mariana-bojan"
+      };
+      firebase.initializeApp(config);
+    }
+    firebase.database().ref("/profile/").once("value").then(s => {
+      this.setState({
+        imageURL: s.val().image,
+        name: s.val().name,
+        description: s.val().description
+      });
+    });
+  }
+
   render() {
+    const { 
+      imageURL,
+      name,
+      description
+    } = this.state;
     return (
       <div className="app">
         <div className="header">
-          <img className="profile" src="https://i0.wp.com/timdettmers.com/wp-content/uploads/2018/05/cropped-profile_300kb.png?fit=512%2C512"/>
+          <img className="profile" src={ imageURL }/>
           <div className="bio">
             <div className="name">
-              Firstname Lastname
+              { name }
             </div>
             <div className="description">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam viverra quis justo sed condimentum. Aenean orci augue, viverra in tincidunt vestibulum, accumsan sit amet ante. Proin gravida augue nisi, eget molestie augue interdum id. Aliquam et felis ac turpis sagittis egestas ut at lectus. Pellentesque tellus ante, scelerisque a diam sit amet, volutpat pellentesque libero. Nullam massa massa, suscipit a luctus quis, tristique at est. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              { description }
             </div>
           </div>
         </div>
