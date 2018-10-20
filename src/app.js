23766c118f35d71fa89a819495164d82438b908e
Akitektuo <alexcopindean@yahoo.com>
2018-10-20 23:09:03 +0300

    Add host code

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './app.css';
 import firebase from 'firebase';
+import Gallery from 'react-grid-gallery';
 
 class App extends Component {
 
@@ -8,21 +9,53 @@ class App extends Component {
     this.setState({
       imageURL: "",
       name: "",
-      description: ""
+      description: "",
+      data: []
     });
     if (!firebase.apps.length) {
-      var config = {
+      let config = {
         databaseURL: "https://mariana-bojan.firebaseio.com/",
         projectId: "mariana-bojan"
       };
       firebase.initializeApp(config);
     }
-    firebase.database().ref("/profile/").once("value").then(s => {
-      this.setState({
-        imageURL: s.val().image,
-        name: s.val().name,
-        description: s.val().description
-      });
+    firebase.database().ref("/profile/").once("value").then(sBio => {
+      const {
+        image,
+        name,
+        description
+      } = sBio.val();
+      firebase.database().ref("/painting/").once("value", sImg => {
+        if (sImg.val() == null) {
+          return;
+        }
+        const data = []
+        sImg.forEach(d => {
+          const {
+            image,
+            width,
+            height,
+            title
+          } = d.val();
+          if (image == "" || width == 0 || height == 0) {
+            return;
+          }
+          data.push({
+            src: image,
+            thumbnail: image,
+            thumbnailWidth: width,
+            thumbnailHeight: height,
+            caption: title,
+            thumbnailCaption: title
+          });
+        });
+        this.setState({
+          imageURL: image,
+          name,
+          description,
+          data
+        });
+      })
     });
   }
 
@@ -44,6 +77,13 @@ class App extends Component {
               { description }
             </div>
           </div>
+        </div>
+        <div className="gallery">
+          <Gallery 
+            rowHeight={512}
+            images={this.state.data}
+            enableImageSelection={false}
+            imageCountSeparator=" din "/>
         </div>
       </div>
     );
